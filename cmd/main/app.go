7617fd8fbc47894c56b21351037bcb181f28b0fa
Threepix <98532056+Threepix@users.noreply.github.com>
2022-08-01 17:07:41 +0300

    ya ebal

---
+++ cmd/main/app.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"log"
+	"net"
+	"net/http"
+	"restapi/cmd/iternal/user"
+	"time"
+)
+
+func main() {
+	log.Println("я ебу собак")
+	router := httprouter.New()
+	log.Println("register user handler")
+	handler := user.NewHandler()
+	handler.Register(router)
+
+	start(router)
+}
+
+func start(router *httprouter.Router) {
+	log.Println("start app")
+	listener, err := net.Listen("tcp", ":1234")
+	if err != nil {
+		panic(err)
+		server := &http.Server{
+			Handler:      router,
+			WriteTimeout: 15 * time.Second,
+			ReadTimeout:  15 * time.Second,
+		}
+		log.Fatalln(server.Serve(listener))
+	}
+}
